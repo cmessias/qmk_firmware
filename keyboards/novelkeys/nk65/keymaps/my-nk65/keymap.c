4abb4f4736e7d503f172ac8dd28900448c135ba5
Caio Messias <caio.messias@pm.me>
2022-07-24 13:06:33 -0300

    Add mod taps to rightmost column

--- keyboards/novelkeys/nk65/keymaps/my-nk65/keymap.c
+++ keyboards/novelkeys/nk65/keymaps/my-nk65/keymap.c
@@ -36,15 +36,15 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [base] = LAYOUT_65_ansi(
     OSL(2),   KC_1,    KC_2,   KC_3,   KC_4,           KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_TILDE,
-    KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,           KC_T,   KC_Y,   SFT_U,  CTL_I,   ALT_O,   GUI_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-    CTL_ESC,  GUI_A,   ALT_S,  CTL_D,  SFT_F,          KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,  KC_END,
+    KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,           KC_T,   KC_Y,   SFT_U,  CTL_I,   ALT_O,   GUI_P,    KC_LBRC, KC_RBRC, KC_BSLS, LCTL_T(KC_HOME),
+    CTL_ESC,  GUI_A,   ALT_S,  CTL_D,  SFT_F,          KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,  LSFT_T(KC_END),
     KC_LSFT,  KC_Z,    KC_X,   KC_C,   KC_V,           KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,          KC_UP,   KC_LGUI,
     KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                         KC_RALT, KC_RCTL, MO(2),    KC_LEFT, KC_DOWN, KC_RGHT),
 
 [gaming] = LAYOUT_65_ansi(
-    TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_TRNS, KC_TRNS, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,          KC_TRNS, KC_TRNS,
+    TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_TRNS, KC_TRNS, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,          KC_TRNS, KC_END,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
